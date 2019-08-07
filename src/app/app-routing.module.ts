51f48301873b6b3cd302c9490e656a31389adc4d
Kim Chai Ly <kimchai.ly@itds.ch>
2019-08-07 21:10:06 +0200

    Add new component containing the anchorscrolling feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+{
+	path: 'heroes',
+	component: HeroComponent,
+	data: { title: 'Heroes List' }
+},
+{
+	path: '',
+	redirectTo: '/heroes',
+	pathMatch: 'full'
+}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, {
+		scrollPositionRestoration: 'enabled',
+		anchorScrolling: 'enabled'
+	})],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+	<router-outlet></router-outlet>
+  `,
+  styles: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app-anchorscrolling';
